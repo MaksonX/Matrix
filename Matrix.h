b0a3eb10e46c21f2bc8dbec1bd0d5c2460d39ba0
MaksonX <100966864+MaksonX@users.noreply.github.com>
2022-12-05 19:49:44 +0300

    Update Matrix.h
    Added a checker to function create(), whether object's rows and columns are filled with constructor Matrix(int, int) input or not.

--- Matrix.h
+++ Matrix.h
@@ -12,11 +12,19 @@ public:
     explicit Matrix(int rows, int cols) :rows(rows), cols(cols) {
         check();
     }
-    void create() {
-        if ((this->rows & this->cols) == 0) {
-            cout << "Haw many rows and cols of your Matrix do you want?: ";
-            cin >> this->rows >> this->cols;
-        }
+    void create();
+    inline void get() const noexcept;
+    void check() const;
+    void erase();
+    void checkOE() const;
+    inline Matrix operator+(Matrix m) const;
+    inline Matrix operator-(Matrix m) const;
+    inline void operator=(Matrix obj);
+    ~Matrix() {}
+};
+
+void Matrix::create() {
+    if (this->rows > 0 && this->cols > 0) {
         cout << "Input digits of matrix: ";
         for (int i = 0; i < this->rows; ++i) {
             vector<int> vv;
@@ -28,104 +36,129 @@ public:
             this->vec.push_back(vv);
         }
     }
-    inline void get() const noexcept {
+    else if (this->rows == 0 && this->cols == 0) {
+        cout << "How many rows and cols do you need for your Matrix?: ";
+        cin >> this->rows >> this->cols;
+        cout << "Input digits of matrix: ";
         for (int i = 0; i < this->rows; ++i) {
+            vector<int> vv;
             for (int j = 0; j < this->cols; ++j) {
-                cout << vec[i][j] << " ";
+                int num;
+                cin >> num;
+                vv.push_back(num);
             }
-            cout << "\n";
+            this->vec.push_back(vv);
+        }
+    }
+}
+
+inline void Matrix::get() const noexcept {
+    for (int i = 0; i < this->rows; ++i) {
+        for (int j = 0; j < this->cols; ++j) {
+            cout << vec[i][j] << " ";
         }
         cout << "\n";
     }
-    void check() const noexcept {
-        if ((this->rows & this->cols) < 0) {
-            terminate;
+    cout << "\n";
+}
+
+void Matrix::check() const {
+    if (this->rows < 0 && this->cols < 0) {
+        throw 1;
+    }
+}
+
+void Matrix::erase() {
+    for (int i = 0; i < this->rows; ++i) {
+        for (int j = 0; j < this->cols; ++j) {
+            this->vec[i][j] = 0;
         }
     }
-    void erase() {
+}
+
+void Matrix::checkOE() const {
+    cout << "What do you want to check? O/E(Odd/Even): ";
+    int count = 0;
+    OE r;
+    char res;
+    cin >> res;
+    if (res == static_cast<char>(OE::odd)) {
+        r = OE::odd;
+    }
+    else {
+        res == static_cast<char>(OE::even) ? r = OE::even : r = OE::odd;
+    }
+    switch (r) {
+    default:
+        cout << "You didn't input valid char!" << endl;
+        break;
+    case OE::even:
+        count = 0;
         for (int i = 0; i < this->rows; ++i) {
             for (int j = 0; j < this->cols; ++j) {
-                this->vec[i][j] = 0;
+                if (this->vec[i][j] % 2 == 0) {
+                    ++count;
+                }
             }
         }
+        if (count == this->rows * this->cols) {
+            cout << "Matrix is even.\n";
+            break;
+        }
+        else {
+            cout << "These members of Matrix are not even: " << (this->rows * this->cols - count) << '\n';
+            break;
+        }
+        break;
+    case OE::odd:
+        count = 0;
+        for (int i = 0; i < this->rows; ++i) {
+            for (int j = 0; j < this->cols; ++j) {
+                if (this->vec[i][j] % 2 != 0) {
+                    ++count;
+                }
+            }
+        }
+        if (count == this->rows * this->cols) {
+            cout << "Matrix is odd.\n";
+            break;
+        }
+        else {
+            cout << "These members of Matrix are not odd: " << (this->rows * this->cols - count) << '\n';
+            break;
+        }
+        break;
     }
-    void checkOE() const {
-        cout << "What do you want to check? O/E(Odd/Even): ";
-        int count = 0;
-        OE r;
-        char res;
-        cin >> res;
-        (static_cast<char>(OE::odd) == res ? r = OE::odd : r = OE::even);
-        switch (r) {
-        case OE::odd:
-            count = 0;
-            for (int i = 0; i < this->rows; ++i) {
-                for (int j = 0; j < this->cols; ++j) {
-                    if (this->vec[i][j] % 2 == 0) {
-                        ++count;
-                    }
-                }
-            }
-            if (count == this->rows * this->cols) {
-                cout << "Matrix is even.\n";
-                break;
-            }
-            else {
-                cout << "These members of Matrix are not even: " << (this->rows * this->cols - count) << '\n';
-                break;
-            }
-            break;
-        case OE::even:
-            count = 0;
-            for (int i = 0; i < this->rows; ++i) {
-                for (int j = 0; j < this->cols; ++j) {
-                    if (this->vec[i][j] % 2 != 0) {
-                        ++count;
-                    }
-                }
-            }
-            if (count == this->rows * this->cols) {
-                cout << "Matrix is odd.\n";
-                break;
-            }
-            else {
-                cout << "These members of Matrix are not odd: " << (this->rows * this->cols - count) << '\n';
-                break;
-            }
-            break;
-        default:
-            cout << "You didn't input valid char!" << endl;
-        }
+}
 
-    }
-    inline Matrix operator+(Matrix m) const {
-        Matrix res(this->rows, this->cols);
-        for (int i = 0; i < res.rows; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < res.cols; ++j) {
-                temp.push_back(this->vec[i][j] + m.vec[i][j]);
-            }
-            res.vec.push_back(temp);
+inline Matrix Matrix::operator+(Matrix m) const {
+    Matrix res(this->rows, this->cols);
+    for (int i = 0; i < res.rows; ++i) {
+        vector<int> temp;
+        for (int j = 0; j < res.cols; ++j) {
+            temp.push_back(this->vec[i][j] + m.vec[i][j]);
         }
-        return res;
+        res.vec.push_back(temp);
     }
-    inline Matrix operator-(Matrix m) const {
-        Matrix res(this->rows, this->cols);
-        for (int i = 0; i < rows; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < cols; ++j) {
-                temp.push_back(this->vec[i][j] - m.vec[i][j]);
-            }
-            res.vec.push_back(temp);
+    return res;
+}
+
+inline Matrix Matrix::operator-(Matrix m) const {
+    Matrix res(this->rows, this->cols);
+    for (int i = 0; i < rows; ++i) {
+        vector<int> temp;
+        for (int j = 0; j < cols; ++j) {
+            temp.push_back(this->vec[i][j] - m.vec[i][j]);
         }
-        return res;
+        res.vec.push_back(temp);
     }
-    inline void operator=(Matrix obj) {
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                this->vec[i][j] = obj.vec[i][j];
-            }
+    return res;
+}
+
+inline void Matrix::operator=(Matrix obj) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            this->vec[i][j] = obj.vec[i][j];
         }
     }
-    ~Matrix() {}
-};
+}
